3f64a3e83ce5c5b48c8ad4b19b4f4ab0feb35e14
NujabesCode <NujabesCode@gmail.com>
2025-03-13 16:41:44 +0900

    메인 컨텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer id='footer' role='contentInfo'>
+      <a href='https://github.com/NujabesCode'
+         rel='noopenner noreferrer'
+         target='_blank'
+      >
+        GitHub
+      </a>
+    </footer>
   )
 }
 
