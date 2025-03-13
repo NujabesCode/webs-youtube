7148faadef5403778f32f1a3698efb2006affe52
NujabesCode <NujabesCode@gmail.com>
2025-03-13 12:46:51 +0900

    컴포넌트 비동기 작업

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Logo = () => {
   return (
-     <h1 className="header__logo">
-                <a href="/">
-                    <em aria-hidden='true'></em>
-                    <span>my<br/>youtube</span>
-                </a>
-     </h1>
+    <h1 className="header__logo">
+        <a href="/">
+            <em aria-hidden='true'></em>
+            <span>my<br/>youtube</span>
+        </a>
+    </h1>
   )
 }
 
