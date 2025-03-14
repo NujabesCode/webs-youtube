7aa11a43072bbf4c3bb464d3b89b4b95ce62823d
NujabesCode <NujabesCode@gmail.com>
2025-03-14 12:45:38 +0900

    추천음악 작업

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import App from './App';
 import './assets/scss/style.scss';
 
 
-
+//const root = ReactDOM.createRoot(document.getElementById('root'))
+// 는 HTML에서 id="root"인 요소를 찾아 React의 루트 컨테이너를 
+// 생성하고, 이 컨테이너를 통해 React 애플리케이션을 렌더링할 
+// 준비를 하는 코드입니다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
